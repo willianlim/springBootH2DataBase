47e23f1db3f3192dc3dc27bac6a1abdbe5c433e1
willianlim <willianrosendodelima@outlook.com>
2022-11-30 07:13:46 -0300

    first commit

---
+++ src/main/java/in/willian/springBootH2DataBase/SpringBootH2DataBaseApplication.java
@@ -0,0 +1,13 @@
+package in.willian.springBootH2DataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DataBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/willian/springBootH2DataBase/SpringBootH2DataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.willian.springBootH2DataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2DataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
