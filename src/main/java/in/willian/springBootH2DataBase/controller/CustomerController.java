778079a0e7d4b84f0f2d2d9ffa347abba369b162
willianlim <willianrosendodelima@outlook.com>
2022-12-03 16:42:11 -0300

    read operation 'GET'

--- src/main/java/in/willian/springBootH2DataBase/controller/CustomerController.java
+++ src/main/java/in/willian/springBootH2DataBase/controller/CustomerController.java
@@ -5,9 +5,12 @@ import in.willian.springBootH2DataBase.repository.ICustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -21,6 +24,19 @@ public class CustomerController {
             return new ResponseEntity<>(iCustomerRepository.save(customer), HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        try {
+            List<Customer> list = iCustomerRepository.findAll();
+            if (list.isEmpty()) {
+                return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
