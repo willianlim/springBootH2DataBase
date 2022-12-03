34ce083e7654ab075c6441e9bb222eb23c9e4def
willianlim <willianrosendodelima@outlook.com>
2022-12-03 16:05:01 -0300

    business model object

---
+++ src/main/java/in/willian/springBootH2DataBase/entity/Customer.java
@@ -0,0 +1,37 @@
+package in.willian.springBootH2DataBase.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Table(name="tbl_customer")
+@Entity
+@Setter
+@Getter
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long age;
+
+    private String location;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+}
